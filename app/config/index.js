fc584b91942f619a573aa75dbb084d70fcff6669
DR <rahmanovdu@gmail.com>
2018-03-30 13:10:17 +0400

    importing untracked file via require

--- app/config/index.js
+++ app/config/index.js
@@ -1,6 +1,12 @@
 import devConfig from './dev.config'
 import prodConfig from './prod.config'
-import untrackedConfig from './untracked.config'
+
+let untrackedConfig = {}
+try {
+  untrackedConfig = require('./untracked.config').default // eslint-disable-line global-require, import/no-unresolved
+} catch (error) {
+  untrackedConfig = {}
+}
 
 const config = Object.assign({}, process.env.NODE_ENV === 'production' ?
  { ...prodConfig, ...untrackedConfig } : { ...devConfig, ...untrackedConfig })
